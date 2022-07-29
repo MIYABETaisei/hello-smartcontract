66e61beb75723ec4469eb650740dcff7068d8c04
MIYABETaisei <mytspe46@gmail.com>
2022-07-29 18:24:33 +0900

    ✨ add SimpleStorage contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,54 @@
-import { ethers } from "hardhat";
+// imports
+const { ethers, run, network } = require("hardhat");
 
+// async main
 async function main() {
-  const Hello = await ethers.getContractFactory("HelloWorld");
-  const hello = await Hello.deploy();
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to: ${simpleStorage.address}`);
+  // what happens when we deploy to our hardhat network?
+  if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, []);
+  }
 
-  await hello.deployed();
-  console.log(
-    `HelloWorld Deploy Tx: https://ropsten.etherscan.io/tx/${hello.deployTransaction.hash}`
-  );
-  console.log(`HelloWorld Contract Address: ${hello.address}`);
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated Value is: ${updatedValue}`);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// async function verify(contractAddress, args) {
+const verify = async (contractAddress: any, args: any) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (e) {
+    if (
+      e instanceof Error &&
+      e.message.toLowerCase().includes("already verified")
+    ) {
+      console.log("Already Verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+// main
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
