34c975fadf51b89b700ad4812d83896cf9e9774c
MIYABETaisei <mytspe46@gmail.com>
2022-07-29 18:23:04 +0900

    ✨ add Greeter.sol

---
+++ contracts/Greeter.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "hardhat/console.sol";
+
+contract Greeter {
+    string private text;
+
+    constructor(string memory _text) {
+        console.log("Deploying a Greeter with text:", _text);
+        text = _text;
+    }
+
+    function show() external view {
+        console.log(text);
+    }
+
+    function set(string memory _setTxt) external {
+        text = _setTxt;
+    }
+}
---
+++ scripts/deploy-greeter.ts
@@ -0,0 +1,19 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Greeter = await ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, Ethereum!");
+
+  await greeter.deployed();
+  console.log(
+    `Greeter Deploy Tx: https://ropsten.etherscan.io/tx/${greeter.deployTransaction.hash}`
+  );
+  console.log(`Greeter Contract Address: ${greeter.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
